cbb0e41f29ba283f7702d95429bc7d5d926e88c7
koharayuki <aquarius3106@icloud.com>
2023-05-29 21:29:15 +0900

    【コメントの修正①】

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
+
 
 
   def index
@@ -12,15 +14,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -41,14 +40,18 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:title, :content, :category_id, :status_id, :delivery_charge_id, :shipping_origin_id, :date_of_shipment_id, :price, :image).merge(user_id: current_user.id)
   end
-end
 
-def move_to_index
-  @item = Item.find(params[:id])
-  unless @item.user_id == current_user.id
-    redirect_to root_path
+  def set_item
+    @item = Item.find(params[:id])
   end
-end
 
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
+end
 
 
