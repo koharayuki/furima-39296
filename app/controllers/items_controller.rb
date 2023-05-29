26aba204178f264e055cfa126d676dbf3381db5e
koharayuki <aquarius3106@icloud.com>
2023-05-29 21:59:04 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: :edit
 
 
@@ -25,6 +25,13 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+       @item.destroy
+      end
+       redirect_to root_path
   end
 
   def create
