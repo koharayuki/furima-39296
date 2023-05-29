978d040d066b3953ba7d6d28b98b6e55139fcec6
koharayuki <aquarius3106@icloud.com>
2023-05-29 12:59:44 +0900

    商品一覧表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,16 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    # @users = User.all
-    # @items = Item.all
+    @users = User.all
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
     @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def create
